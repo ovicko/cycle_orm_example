e6c0655bdfe8df6f30aeaf7e52a921c592cbdaf8
AMWOLLO VICTOR <amwollo@yahoo.com>
2021-07-17 16:07:16 +0300

    Test table prefix

--- index.php
+++ index.php
@@ -7,7 +7,10 @@ $dbal = new Database\DatabaseManager(
     new Database\Config\DatabaseConfig([
         'default'     => 'default',
         'databases'   => [
-            'default' => ['connection' => 'mysql']
+            'default' => [
+                'connection' => 'mysql',
+                'prefix'         => 'oc_'
+            ]
         ],
         'connections' => [
             'mysql'     => [
@@ -24,11 +27,12 @@ $dbal = new Database\DatabaseManager(
 
 // print_r($dbal->database('default')->getTables());
 //test database access
-$customers = $dbal->database('default')->table('oc_customer')->select()->fetchAll();
+$customers = $dbal->database('default')->table('customer')->select()->fetchAll();
 
 foreach ($customers as $customer) {
     // print_r($customer);
     print_r($customer['customer_id'] . " " . $customer['firstname']);
+    print_r("\n");
 }
 
 
