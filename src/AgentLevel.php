bc8e1168acf5cb682c62d3290cffa90046790104
AMWOLLO VICTOR <amwollo@yahoo.com>
2021-07-17 16:52:23 +0300

    Add getters and setters

--- src/AgentLevel.php
+++ src/AgentLevel.php
@@ -2,7 +2,7 @@
 namespace Ovicko\CycleOrmExample;
 
 use Cycle\Annotated\Annotation\Entity;
-
+use Cycle\Annotated\Annotation\Column;
 /**
  * @Entity(
  *     table    = "agent_level"
@@ -21,4 +21,40 @@ class AgentLevel
 
     /** @Column(type = "integer", name = "status") */
     protected $status;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getRate(): float
+    {
+        return $this->rate;
+    }
+
+    public function setRate(float $rate): void
+    {
+        $this->rate = $rate;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): void
+    {
+        $this->status = $status;
+    }
+
 }
